abc799a15ee5ff5c4d0caaf97e9a2f3b3d329397
chopsticks35 <tiwegmeyeryoga@gmail.com>
2016-01-18 11:25:48 -0700

    first commit of string reporter simpleprogram in js

---
+++ script.js
@@ -0,0 +1,9 @@
+var word = window.prompt("Please enter any word:");
+alert(word + " is your word");
+alert("There are " + word.length + " characters in your word");
+alert("The third character in your word is: " + word.charAt(2));
+alert("This is your word in lowercase: " + word.toLowerCase());
+alert("This is your word in uppercase: " + word.toUpperCase());
+alert(word.substring(1,4) + " is the subword of your word");
+
+ 
